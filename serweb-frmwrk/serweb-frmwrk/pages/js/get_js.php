93c0f67d80b1926c0cf258098296b27efea244d4
Karel Kozlík <kozlik@kufr.cz>
2021-04-13 15:08:07 +0200

    Fix 'content-type' header returned by get_js.php script

--- serweb-frmwrk/serweb-frmwrk/pages/js/get_js.php
+++ serweb-frmwrk/serweb-frmwrk/pages/js/get_js.php
@@ -4,7 +4,7 @@
  *	$Id: get_js.php,v 1.1 2005/08/22 14:35:12 kozlik Exp $
  */
 
-Header("content-type: text/js");
+Header("content-type: application/javascript");
 
 /* Load the file specified in SERWEB_AUTO_PREPEND environment variable */
 if (getenv('SERWEB_AUTO_PREPEND')) {
